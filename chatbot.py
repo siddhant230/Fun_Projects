a119600166b24b6e46af284b92936f0c85230316
Siddhant Rai <35813591+siddhant230@users.noreply.github.com>
2019-12-07 13:05:20 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,62 +1,130 @@
-from tkinter import *
-import tkinter as tk
-import random
-from PIL import Image,ImageTk
-from functools import partial
+import random,os
 import speech_recognition as sr
+from selenium import webdriver
+import wolframalpha,gtts,pyttsx3
+import datetime,wikipedia,time
 
-root=tk.Tk()
-root.geometry("450x360")
-root.title('BOT')
+engine=pyttsx3.init('sapi5')
+client=wolframalpha.Client('JGQG46-Y5WTEL7J5G')
+voice=engine.getProperty('voices')
+engine.setProperty('voices',voice[len(voice)-1].id)
+engine.setProperty('rate', 200)
+options = webdriver.ChromeOptions()
 
-def reply(text_box):
+###########model here#######################
+def reply(text_inp):
     text=random.choice(['hey there','how are you??','may i help you??','ha ha LOL','I am fine...'])
-    print(text_box.get(1.0,END))
-    Label(root,text=text,bg='black',fg='white',font=("verdana",18, "bold"),width=21).place(relx=0.0,rely=0.35)
-    text_box.delete(1.0,END)
+    speak(text)
+############model here#####################
 
+def speak(text):
+    print('BOT : '+text)
+    engine.say(text)
+    engine.runAndWait()
+c=0
 def listen():
+    global c
+    speak('How can I help?')
     r=sr.Recognizer()
+    text=''
     with sr.Microphone() as source:
-        Label(root,text='LISTENING...',bg='black',fg='white',font=("verdana",18, "bold"),width=21).place(relx=0.0,rely=0.35)
         audio=r.listen(source)
         try:
             text=r.recognize_google(audio)
         except:
-            Label(root,text='NOT CLEAR, Please Say again..',bg='black',fg='white',font=("verdana",18, "bold"),width=21).place(relx=0.0,rely=0.35)
-        Label(root,text='working...',bg='black',fg='white',font=("verdana",18, "bold"),width=21).place(relx=0.0,rely=0.35)
-        print(text)
-        #return text
+            c+=1
+            err="I can't understand what you said sir,Please try again..."
+            speak(err)
+            if c==2:
+                err="Try writing your query..."
+                speak(err)
+                text=input('YOU : ')
+    if text!='':
+        c=0
+        print('YOU : {}'.format(text))
+        commands=text.split(' ')
+        for i in range(len(commands)):
+            commands[i]=commands[i].lower()
+        if 'wait' in commands:
+            speak('I am waiting...')
+            time.sleep(5)
+            speak('I waited for 5 seconds...')
+        elif 'f***' in commands:
+            speak('denge pel khatam ho jayga khel...')
+            speak('Please atleast talk good, kharaab to apki shakal bhi hai...(#offence Intended)')
+        elif 'weather' in commands:
+            weather(commands)
+        elif 'play' in commands:
+            play_song(commands)
+        elif 'open' in commands or 'run' in commands:
+            run_command(commands)
+        elif 'search' in commands or 'what' in commands or 'joke' in commands:
+            search_command(commands)
+        else:
+            reply(text)
 
+def play_song(commands):
+    id='https://www.soundcloud.com/search/sounds?q='
+    driver=webdriver.Chrome(executable_path='C:\\Users\\tusha\\Downloads\\chromedriver.exe',options=options)
+    name='%20'.join(commands[1:])
+    driver.get(id+name)
+    try:
+        driver.find_element_by_class_name('search__empty')
+        speak('No such song found sir,Try again...')
+        driver.quit()
+    except:
+        button=driver.find_element_by_class_name('soundTitle__playButton')
+        button.click()
 
-#############header construction#########
-head_frame=Frame(root,width=700,height=70,bg='green')
-img=ImageTk.PhotoImage(Image.open('C:\\Users\\tusha\Downloads\\bot.png'))
-icon=Label(head_frame,image=img,bg='green')
-icon.place(relx=0.003,rely=0.03)
-header=Label(head_frame,text='CHATBOT',width=50,bg='green',fg='white',font=("verdana", 20, "bold"),anchor='w')
-header.place(relx=0.07,rely=0)
-Online=Label(head_frame,text='Online',bg='green',fg='white',font=("verdana", 8, "bold"),anchor='w')
-Online.place(relx=0.07,rely=0.64)
-head_frame.place(relx=0,rely=0)
-###############header done##############
+def weather(commands):
+    query=' '.join(commands)
+    speak("Searching for {}".format(query))
+    result=client.query(query)
+    opt=next(result.results).text
+    speak(opt)
 
-###############LABEL AT CENTER#########
-body_frame=Frame(root,width=450,height=360,bg='black')
-body_frame.place(relx=0,rely=0.184)
+def search_command(commands):
+    if 'joke' in commands:
+        query='tell me a joke'
+        result=client.query(query)
+        opt=next(result.results).text
+        opt=opt.split('(')[0]
+    else:
+        speak('searching...')
+        query=' '.join(commands[1:])
+        try:
+            try:
+                "BOT : Sure Sir!\nSearching for {}".format(query)
+                speak("Searching for {}".format(query))
+                result=client.query(query)
+                opt=next(result.results).text
+                speak("Here is what I found\nInternet says...")
+                speak(opt)
+            except:
+                result=wikipedia.summary(query,sentences=3)
+                speak("Here is what I found\nAccording to Wikipedia...")
+                speak(result)
+        except:
+            browser=webdriver.Chrome(executable_path='C:\\Users\\tusha\\Downloads\\chromedriver.exe',options=options)
+            speak("Here is what I found on Google...")
+            browser.get("https://www.google.com/search?q=" + query + "&start=" + str(10 * 0))
 
-#############making the input frame###########
-footer_frame=Frame(root,width=700,height=100,bg='green')
-text_box=Text(footer_frame,fg='black',font=("verdana", 15, "bold"),width=25,height=3)
-text_box.place(relx=0.013,rely=0.070)
-send=PhotoImage(file='C:\\Users\\tusha\Downloads\\send.png')
-send_button=Button(footer_frame,image=send,command=partial(reply,text_box))
-send_button.place(relx=0.53,rely=0.126)
-mic=PhotoImage(file='C:\\Users\\tusha\Downloads\\mic.png')
-mic_button=Button(root,image=mic,command=listen,width=50,height=50)
-mic_button.place(relx=0.85,rely=0.55)
-footer_frame.place(relx=0,rely=0.72)
-#############input frame done################
-reply(Text())
-root.mainloop()
+def run_command(commands):
+    speak('Opening {}'.format(''.join(commands[1:])))
+    driver=webdriver.Chrome(executable_path='C:\\Users\\tusha\\Downloads\\chromedriver.exe',options=options)
+    driver.get('https://www.{}.com'.format(''.join(commands[1:])))
 
+if __name__=='__main__':
+    mode='#'
+    times=datetime.datetime.now().hour
+    if times>=0 and times<12:
+        speak('Good Morning Sir!')
+    elif times>=12 and times<=16:
+        speak('Good Afternoon Sir!')
+    elif times>16 and times<24 and times!=0:
+        speak('Good Evening Sir!')
+    else:
+        speak("It's already Night")
+
+    while mode=='#':
+        listen()
