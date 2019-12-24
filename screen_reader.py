3b577c6c9939753692bf588067d35091e9f21c9a
Siddhant Rai <35813591+siddhant230@users.noreply.github.com>
2019-12-25 00:30:44 +0530

    Update screen_reader.py

--- screen_reader.py
+++ screen_reader.py
@@ -1,12 +1,13 @@
 import pytesseract
 import cv2
 import playsound
-import pyttsx3
+import pyttsx3,urllib.request
 import pyscreenshot as imageGrab
 import numpy as np
 
-def sound_player(path):
-    playsound.playsound(path)
+##########keeping tesseract in path###########
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+##############################################
 def speaker(text):
     '''obj=gtts.gTTS(text=text,lang='en',slow=False)'''  #you need to have pyttsx3 installed along with libspeak
                                           #otherwise you can simply use gtts; you just need internet connection for it.
@@ -17,24 +18,26 @@ def speaker(text):
     engine.runAndWait()
 
 def helper():
-    img=cv2.imread('/home/parmeet/Downloads/saved.png')
+    img=cv2.imread('saved.png')
     text=pytesseract.image_to_string(img,lang='eng')
     print(text)
     speaker(text)
 
 count=0
+cap=cv2.VideoCapture('http://172.21.160.127:8080/shot.jpg')
+url='http://172.21.160.127:8080/shot.jpg'
 while True:
     if count%30==0 and count!=0:
-        cv2.imwrite('/home/parmeet/Downloads/saved.png',img)
+        cv2.imwrite('saved.png',img)
+        ####tesseract helper function
         helper()
     count+=1
     print(count)
-    img=imageGrab.grab()
-    img=np.array(img)
+    ############picking up the image and converting it to usable form numpy array.
+    imgResp = urllib.request.urlopen(url)
+    imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    img = cv2.imdecode(imgNp,-1)
+    cv2.imshow('img',img)
     width,height,dim=img.shape
-    print(img.shape)
-    img=img[200:height,0:1366]
-    print(img.shape)
-
     if cv2.waitKey(1)==ord('q'):
         break
